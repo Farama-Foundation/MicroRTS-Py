791f9161711a8de1f8fd4ee42586669b6e8c46d5
Costa Huang <costa.huang@outlook.com>
2019-05-27 19:08:54 -0400

    Master # Update experiments

--- experiments/stable_DQN.py
+++ experiments/stable_DQN.py
@@ -1,8 +1,8 @@
 import gym
 import gym_microrts
-from stable_baselines.common.policies import MlpPolicy
+from stable_baselines.deepq.policies import MlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
-from stable_baselines import A2C
+from stable_baselines import DQN
 from stable_baselines.common import set_global_seeds
 set_global_seeds(0)
 env = gym.make("Microrts-v0")
@@ -10,7 +10,7 @@ env = gym.make("Microrts-v0")
 env.set_map_dimension(4, 4)
 env = DummyVecEnv([lambda: env])  # The algorithms require a vectorized environment to run
 
-model = A2C(MlpPolicy, env, verbose=1)
+model = DQN(MlpPolicy, env, tensorboard_log="./", verbose=1)
 model.learn(total_timesteps=1000000)
 
 obs = env.reset()
