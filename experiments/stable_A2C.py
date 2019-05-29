5787eaabd212770b3c3c135e679b70f0a0c1cbed
Costa Huang <costa.huang@outlook.com>
2019-05-29 17:41:32 -0400

    Master # Update stable_a2c

--- experiments/stable_A2C.py
+++ experiments/stable_A2C.py
@@ -1,22 +1,24 @@
 import gym
 import gym_microrts
+import wandb
 from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines import A2C
 from stable_baselines.common import set_global_seeds
-set_global_seeds(0)
+
+wandb.init(project="MicrortsRL", tensorboard=True)
+config = wandb.config
+config.seed = 0
+config.dimension_x = 4
+config.dimension_y = 4
+config.total_timesteps = 20000000
+
+set_global_seeds(config.seed)
 env = gym.make("Microrts-v0")
-# env.init(16, 16)
-env.init(4, 4)
+env.init(config.dimension_x , config.dimension_y)
 env = DummyVecEnv([lambda: env])  # The algorithms require a vectorized environment to run
 
-model = A2C(MlpPolicy, env, tensorboard_log="./", verbose=1)
-model.learn(total_timesteps=1000000)
-model.save("/tmp/a2c")
-
-obs = env.reset()
-rews = []
-for i in range(1000):
-    action, _states = model.predict(obs)
-    obs, rewards, dones, info = env.step(action)
-    rews += [rewards[0]]
+model = A2C(MlpPolicy, env, verbose=1)
+model.learn(total_timesteps=config.total_timesteps)
+model.save("a2c.model")
+wandb.save("a2c.model")
