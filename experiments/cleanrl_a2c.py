74a4159df75b22667bf36c4bfe758125294a4672
Costa Huang <costa.huang@outlook.com>
2019-06-09 13:19:51 -0400

    Master # Setup production mode

--- experiments/cleanrl_a2c.py
+++ experiments/cleanrl_a2c.py
@@ -16,7 +16,9 @@ import random
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='A2C agent')
     # Common arguments
-    parser.add_argument('--gym-id', type=str, default="CartPole-v0",
+    parser.add_argument('--exp-name', type=str, default="dummy",
+                       help='the name of this experiment')
+    parser.add_argument('--gym-id', type=str, default="Microrts-v0",
                        help='the id of the gym environment')
     parser.add_argument('--learning-rate', type=float, default=7e-4,
                        help='the learning rate of the optimizer')
@@ -24,10 +26,10 @@ if __name__ == "__main__":
                        help='seed of the experiment')
     parser.add_argument('--episode-length', type=int, default=2000,
                        help='the maximum length of each episode')
-    parser.add_argument('--total-timesteps', type=int, default=50000,
+    parser.add_argument('--total-timesteps', type=int, default=5000,
                        help='total timesteps of the experiments')
-    parser.add_argument('--torch-deterministic', type=bool, default=True,
-                       help='whether to set `torch.backends.cudnn.deterministic=True`')
+    parser.add_argument('--prod-mode', type=bool, default=False,
+                       help='run the script in production mode and use wandb to log outputs')
     
     # Algorithm specific arguments
     parser.add_argument('--gamma', type=float, default=0.99,
@@ -39,9 +41,11 @@ if __name__ == "__main__":
     parser.add_argument('--ent-coef', type=float, default=0.01,
                        help="policy entropy's coefficient the loss function")
     args = parser.parse_args()
+    if not args.seed:
+        args.seed = int(time.time())
 
 # TRY NOT TO MODIFY: setup the environment
-env = gym.make("Microrts-v0")
+env = gym.make(args.gym_id)
 config = gym_microrts.types.Config(
     ai1_type="no-penalty",
     ai2_type="passive",
@@ -50,10 +54,11 @@ config = gym_microrts.types.Config(
     client_port=9898,
     microrts_path="E:/Go/src/github.com/vwxyzjn/201906051646.microrts"
 )
-
+if args.prod_mode:
+    import wandb
+    config.render = False
+    config.microrts_path = "/root/microrts"
 env.init(config)
-if not args.seed:
-    args.seed = int(time.time())
 random.seed(args.seed)
 np.random.seed(args.seed)
 torch.manual_seed(args.seed)
@@ -95,11 +100,13 @@ optimizer = optim.Adam(list(pg.parameters()) + list(vf.parameters()), lr=args.le
 loss_fn = nn.MSELoss()
 
 # TRY NOT TO MODIFY: start the game
-experiment_name = "".join(
-        [time.strftime('%Y.%m.%d.%H.%M.%z')] + 
-        [ f"__{getattr(args, arg)}" for arg in vars(args)]
-)
+experiment_name = f"{time.strftime('%b%d_%H-%M-%S')}__{args.exp_name}__{args.seed}"
 writer = SummaryWriter(f"runs/{experiment_name}")
+writer.add_text('hyperparameters', "|param|value|\n|-|-|\n%s" % (
+        '\n'.join([f"|{key}|{value}|" for key, value in vars(args).items()])))
+if args.prod_mode:
+    wandb.init(project="MicrortsRL", tensorboard=True, config=vars(args))
+    writer = SummaryWriter(f"/tmp/{experiment_name}")
 global_step = 0
 while global_step < args.total_timesteps:
     next_obs = np.array(env.reset())
@@ -127,6 +134,12 @@ while global_step < args.total_timesteps:
         next_obs = np.array(next_obs)
         if dones[step]:
             break
+        
+        if args.prod_mode and global_step % 20000 == 0:
+            torch.save(pg.state_dict(), "/tmp/pg.pt")
+            torch.save(vf.state_dict(), "/tmp/vf.pt")
+            wandb.save("/tmp/pg.pt")
+            wandb.save("/tmp/vf.pt")
     
     # TODO: training.
     # calculate the discounted rewards, or namely, returns
@@ -149,3 +162,4 @@ while global_step < args.total_timesteps:
     writer.add_scalar("charts/episode_reward", rewards.sum(), global_step)
     writer.add_scalar("charts/global_step", global_step, global_step)
 env.close()
+writer.close()
--- experiments/random_agent.py
+++ experiments/random_agent.py
@@ -11,6 +11,8 @@ import random
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='A2C agent')
     # Common arguments
+    parser.add_argument('--exp-name', type=str, default="dummy",
+                       help='the name of this experiment')
     parser.add_argument('--gym-id', type=str, default="Microrts-v0",
                        help='the id of the gym environment')
     parser.add_argument('--learning-rate', type=float, default=7e-4,
@@ -19,12 +21,16 @@ if __name__ == "__main__":
                        help='seed of the experiment')
     parser.add_argument('--episode-length', type=int, default=2000,
                        help='the maximum length of each episode')
-    parser.add_argument('--total-timesteps', type=int, default=50000,
+    parser.add_argument('--total-timesteps', type=int, default=5000,
                        help='total timesteps of the experiments')
+    parser.add_argument('--prod-mode', type=bool, default=False,
+                       help='run the script in production mode and use wandb to log outputs')
     args = parser.parse_args()
+    if not args.seed:
+        args.seed = int(time.time())
 
 # TRY NOT TO MODIFY: setup the environment
-env = gym.make("Microrts-v0")
+env = gym.make(args.gym_id)
 config = gym_microrts.types.Config(
     ai1_type="penalty",
     ai2_type="passive",
@@ -33,20 +39,23 @@ config = gym_microrts.types.Config(
     client_port=9898,
     microrts_path="E:/Go/src/github.com/vwxyzjn/201906051646.microrts"
 )
-
+if args.prod_mode:
+    import wandb
+    config.render = False
+    config.microrts_path = "/root/microrts"
 env.init(config)
-if not args.seed:
-    args.seed = int(time.time())
 random.seed(args.seed)
 np.random.seed(args.seed)
 env.seed(args.seed)
 
 # TRY NOT TO MODIFY: start the game
-experiment_name = "".join(
-        [time.strftime('%Y.%m.%d.%H.%M.%z')] + 
-        [ f"__{getattr(args, arg)}" for arg in vars(args)]
-)
+experiment_name = f"{time.strftime('%b%d_%H-%M-%S')}__{args.exp_name}__{args.seed}"
 writer = SummaryWriter(f"runs/{experiment_name}")
+writer.add_text('hyperparameters', "|param|value|\n|-|-|\n%s" % (
+        '\n'.join([f"|{key}|{value}|" for key, value in vars(args).items()])))
+if args.prod_mode:
+    wandb.init(project="MicrortsRL", tensorboard=True, config=vars(args))
+    writer = SummaryWriter(f"/tmp/{experiment_name}")
 global_step = 0
 while global_step < args.total_timesteps:
     next_obs = np.array(env.reset())
@@ -70,3 +79,4 @@ while global_step < args.total_timesteps:
     writer.add_scalar("charts/episode_reward", rewards.sum(), global_step)
     writer.add_scalar("charts/global_step", global_step, global_step)
 env.close()
+writer.close()
