0757378e8505afb7938b103e666fb980f2bb5596
Costa Huang <costa.huang@outlook.com>
2020-01-15 00:16:31 -0500

    Master # update

--- evaluation.bash
+++ evaluation.bash
@@ -5,39 +5,54 @@ export MKL_NUM_THREADS=$NUM_CORES OMP_NUM_THREADS=$NUM_CORES
 for seed in {1..2}
 do
     (sleep 0.3 && nohup python cleanrl_a2c.py \
-    --prod-mode True \
-    --seed $seed \
     --total-timesteps 2000000 \
     --wandb-project-name gym-microrts \
-    --gym-id MicrortsGlobalAgentsMaxResources4x4Prod-v0) >& /dev/null &
+    --gym-id MicrortsGlobalAgentsMaxResources4x4Prod-v0 \
+    --prod-mode True \
+    --capture-video True \
+    --seed $seed) >& /dev/null &
+done
+
+for seed in {1..2}
+do
+    (sleep 0.3 && nohup python cleanrl_a2c_mask.py \
+    --total-timesteps 2000000 \
+    --wandb-project-name gym-microrts \
+    --gym-id MicrortsGlobalAgentsMaxResources4x4Prod-v0 \
+    --prod-mode True \
+    --capture-video True \
+    --seed $seed) >& /dev/null &
 done
 
 for seed in {1..2}
 do
     (sleep 0.3 && nohup python cleanrl_a2c.py \
-    --prod-mode True \
-    --seed $seed \
     --total-timesteps 2000000 \
     --wandb-project-name gym-microrts \
-    --gym-id MicrortsGlobalAgentsMaxResources6x6Prod-v0) >& /dev/null &
+    --gym-id MicrortsGlobalAgentsMaxResources6x6Prod-v0 \
+    --prod-mode True \
+    --capture-video True \
+    --seed $seed) >& /dev/null &
 done
 
 for seed in {1..2}
 do
     (sleep 0.3 && nohup python cleanrl_a2c.py \
-    --prod-mode True \
-    --seed $seed \
     --total-timesteps 2000000 \
     --wandb-project-name gym-microrts \
-    --gym-id MicrortsLocalAgentsMaxResources4x4Window1Prod-v0) >& /dev/null &
+    --gym-id MicrortsLocalAgentsMaxResources4x4Window1Prod-v0 \
+    --prod-mode True \
+    --capture-video True \
+    --seed $seed) >& /dev/null &
 done
 
 for seed in {1..2}
 do
     (sleep 0.3 && nohup python cleanrl_a2c.py \
-    --prod-mode True \
-    --seed $seed \
     --total-timesteps 2000000 \
     --wandb-project-name gym-microrts \
-    --gym-id MicrortsLocalAgentsMaxResources6x6Window1Prod-v0) >& /dev/null &
+    --gym-id MicrortsLocalAgentsMaxResources6x6Window1Prod-v0 \
+    --prod-mode True \
+    --capture-video True \
+    --seed $seed) >& /dev/null &
 done
