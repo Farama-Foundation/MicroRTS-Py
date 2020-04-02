fix. it works now. don't use softmax

gym_microrts/envs/param_op_env.py
@@ -58,10 +58,11 @@ class ParamOpEnv(gym.Env):
         self.init_properties()
 
     def init_properties(self):
-        self.dummy_obs = np.ones(500)
+        dummy_number = 10
+        self.dummy_obs = np.ones(dummy_number)
         self.observation_space = spaces.Box(low=0.0,
             high=1.0,
-            shape=(500,),)
+            shape=(dummy_number,),)
         self.action_space = spaces.Box(low=0.0,
             high=1.0,
             shape=(6,))
@@ -71,8 +72,8 @@ class ParamOpEnv(gym.Env):
         return PlayoutPolicyOptimization()
 
     def step(self, action, raw=False):
-        action = softmax(action)
-        response = self.client.computeRandomAIWinrate(JArray(JDouble)(action), 0, 3, os.path.expanduser(self.config.microrts_path))
+        # action = softmax(action)
+        response = self.client.computeRandomAIWinrate(JArray(JDouble)(action), 0, 1, os.path.expanduser(self.config.microrts_path))
         return self.dummy_obs, response, False, {}
 
     def reset(self, raw=False):

test_param_op.py
@@ -10,6 +10,11 @@ from jpype.imports import registerDomain
 import jpype.imports
 from jpype.types import *
 
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum(axis=0) # only difference
+
 if "ParamOpEnv-v0" not in gym.envs.registry.env_specs:
     register(
         "ParamOpEnv-v0",
@@ -21,13 +26,14 @@ if "ParamOpEnv-v0" not in gym.envs.registry.env_specs:
             map_path="maps/base4x5.xml",
             # below are dev properties
             microrts_path="~/Documents/work/go/src/github.com/vwxyzjn/microrts",
-        )}
+        )},
+        max_episode_steps=1
     )
 
 env = gym.make("ParamOpEnv-v0")
 env.action_space.seed(0)
 try:
-    obs = env.reset(True)
+    obs = env.reset()
 except Exception as e:
     e.printStackTrace()
 