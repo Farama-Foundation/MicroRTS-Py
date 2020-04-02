b7aad5adb82de3708ca22125496c2c362c1329b1
Costa Huang <costa.huang@outlook.com>
2020-04-02 13:29:25 -0400

    add dependency

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='gym_microrts',
       version='0.0.1',
-      install_requires=['gym', 'dacite', 'jPype1']
+      install_requires=['gym', 'dacite', 'jPype1', 'hilbertcurve']
 )
