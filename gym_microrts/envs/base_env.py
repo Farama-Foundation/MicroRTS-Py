d598b397cbaaf2e4c8b85ee7f1f909200a38f6f0
Costa Huang <costa.huang@outlook.com>
2020-04-24 15:56:57 -0400

    jpype-project/jpype#133 fix multidimensional array

--- gym_microrts/envs/base_env.py
+++ gym_microrts/envs/base_env.py
@@ -66,14 +66,14 @@ class BaseSingleAgentEnv(gym.Env):
         action = np.array([action])
         response = self.client.step(action, self.config.frame_skip)
         if raw:
-            return convert3DJarrayToNumpy(response.observation), response.reward[:], response.done[:], json.loads(str(response.info))
-        return self._encode_obs(convert3DJarrayToNumpy(response.observation)), response.reward[:], response.done[:], json.loads(str(response.info))
+            return np.array(response.observation), response.reward[:], response.done[:], json.loads(str(response.info))
+        return self._encode_obs(np.array(response.observation)), response.reward[:], response.done[:], json.loads(str(response.info))
 
     def reset(self, raw=False):
         response = self.client.reset()
         if raw:
-            return convert3DJarrayToNumpy(response.observation)
-        return self._encode_obs(convert3DJarrayToNumpy(response.observation))
+            return np.array(response.observation)
+        return self._encode_obs(np.array(response.observation))
 
     def render(self, mode='human'):
         if mode=='human':
@@ -93,18 +93,3 @@ class BaseSingleAgentEnv(gym.Env):
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
-
-def convert3DJarrayToNumpy(jArray):
-    # get shape
-    arr_shape = (len(jArray),)
-    temp_array = jArray[0]
-    while hasattr(temp_array, '__len__'):
-        arr_shape += (len(temp_array),)
-        temp_array = temp_array[0]
-    arr_type = type(temp_array)
-    # transfer data
-    resultArray = np.empty(arr_shape, dtype=arr_type)
-    for ix in range(arr_shape[0]):
-        for i,cols in enumerate(jArray[ix][:]):
-            resultArray[ix][i,:] = cols[:]
-    return resultArray
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
 setup(name='gym_microrts',
-      version='0.0.1',
+      version='0.1.0',
       install_requires=['gym', 'dacite', 'jPype1', 'hilbertcurve']
 )
