09f74e005de4e0f7684d0c11d59952517cc3ac24
Costa Huang <costa.huang@outlook.com>
2020-05-25 07:24:51 -0400

    add files

--- docker/aws.py
+++ docker/aws.py
@@ -17,35 +17,44 @@ runs_match = re.findall('(python)(.+)((?:\n.+)+)(seed)',strings)
 for run_match in runs_match:
     run_match_str = "".join(run_match).replace("\\\n", "")
     # print(run_match_str)
-    for seed in range(2):
+    for seed in range(1,5):
         final_run_cmds += [run_match_str.replace("$seed", str(seed)).split()]
 
+# use docker directly
+# cores = 40
+# repo = "vwxyzjn/gym-microrts_shared_memory:latest"
+# current_core = 0
+# for final_run_cmd in final_run_cmds:
+#     print(f'docker run -d --shm-size="500m" --cpuset-cpus="{current_core}" -e WANDB={wandb_key} {repo} ' + " ".join(final_run_cmd))
+#     current_core = (current_core + 1) % cores
+
 # submit jobs
-for final_run_cmd in final_run_cmds:
-    job_name = re.findall('(python)(.+)(.py)'," ".join(final_run_cmd))[0][1].strip() + str(int(time.time()))
-    job_name = job_name.replace("/", "_").replace("_param ", "")
-    response = client.submit_job(
-        jobName=job_name,
-        jobQueue='gym-microrts',
-        jobDefinition='gym-microrts',
-        containerOverrides={
-            'vcpus': 1,
-            'memory': 1000,
-            'command': final_run_cmd,
-            'environment': [
-                {
-                    'name': 'WANDB',
-                    'value': wandb_key
-                }
-            ]
-        },
-        retryStrategy={
-            'attempts': 1
-        },
-        timeout={
-            'attemptDurationSeconds': 16*60*60 # 16 hours
-        }
-    )
-    if response['ResponseMetadata']['HTTPStatusCode'] != 200:
-        print(response)
-        raise Exception("jobs submit failure")
+# for final_run_cmd in final_run_cmds:
+#     job_name = re.findall('(python)(.+)(.py)'," ".join(final_run_cmd))[0][1].strip() + str(int(time.time()))
+#     job_name = job_name.replace("/", "_").replace("_param ", "")
+#     response = client.submit_job(
+#         jobName=job_name,
+#         jobQueue='gym-microrts',
+#         jobDefinition='gym-microrts',
+#         containerOverrides={
+#             'vcpus': 1,
+#             'memory': 1000,
+#             'command': final_run_cmd,
+#             'environment': [
+#                 {
+#                     'name': 'WANDB',
+#                     'value': wandb_key
+#                 }
+#             ]
+#         },
+#         retryStrategy={
+#             'attempts': 1
+#         },
+#         timeout={
+#             'attemptDurationSeconds': 16*60*60 # 16 hours
+#         }
+#     )
+#     if response['ResponseMetadata']['HTTPStatusCode'] != 200:
+#         print(response)
+#         raise Exception("jobs submit failure")docker run -d --shm-size="500m" --cpuset-cpus="13" -e WANDB=6603a1e99a016ac5002729a06b08e13931d4ee02 vwxyzjn/gym-microrts_shared_memory:latest python mask/ppo_no_mask_10x10.py --exp-name ppo_no_mask --total-timesteps 500000 --wandb-project-name gym-microrts-mask2 --gym-id MicrortsMining10x10F9-v0 --no-cuda --gae --norm-obs --norm-adv --anneal-lr --clip-vloss --invalid-action-penalty -0.01 --prod-mode --capture-video --seed 2
+
---
+++ experiments/mask/illustration_of_probs.py
@@ -0,0 +1,26 @@
+# suppose action 1 is invalid
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.distributions.categorical import Categorical
+device = "cpu"
+
+# no invalid action masking
+print("=============regular=============")
+target_logits = torch.tensor([1., 1., 1., 1.,] , requires_grad=True)
+target_probs = Categorical(logits=target_logits)
+action = target_probs.sample()
+print(target_probs.probs)
+
+# invalid action masking via logits
+print("==================invalid action masking=============")
+target_logits = torch.tensor([1., 1., 1., 1.,] , requires_grad=True)
+invalid_action_masks = torch.tensor([1., 1., 0., 1.,])
+invalid_action_masks = invalid_action_masks.type(torch.BoolTensor)
+adjusted_logits = torch.where(invalid_action_masks, 
+                              target_logits, torch.tensor(-1e+8))
+adjusted_probs = Categorical(logits=adjusted_logits)
+action = adjusted_probs.sample()
+print(adjusted_probs.probs)
