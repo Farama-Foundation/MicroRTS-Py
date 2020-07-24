ca79797dd2f4f5a6fb2ed798b43a0293e719772e
Costa Huang <costa.huang@outlook.com>
2020-07-24 19:37:57 -0400

    add experimental dqn

--- hello_world.py
+++ hello_world.py
@@ -1,15 +1,18 @@
 import gym
 import gym_microrts
 import time
+from gym.wrappers import TimeLimit, Monitor
+
 try:
     env = gym.make("MicrortsGlobalAgentRandomEnemy10x10FrameSkip9-v0")
+    env = Monitor(env, f'videos')
     env.action_space.seed(0)
     env.reset()
     for i in range(10000):
-        env.render()
         time.sleep(0.2)
         obs = env.step(env.action_space.sample())
-        if env.step(env.action_space.sample())[2]:
+        next_obs, reward, done, info = env.step(env.action_space.sample())
+        if done:
             print("done")
             break
     #env.close()
--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='gym_microrts',
       version='0.1.0',
-      install_requires=['gym', 'dacite', 'jPype1', 'hilbertcurve']
+      install_requires=['gym', 'dacite', 'jPype1']
 )
