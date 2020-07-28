e33e97ed8cf5c1304edcf7d397e5197f87e8f2f4
Costa Huang <costa.huang@outlook.com>
2020-07-28 10:51:37 -0400

    update docker related files

--- gym_microrts/envs/__init__.py
+++ gym_microrts/envs/__init__.py
@@ -15,5 +15,4 @@ from gym_microrts.envs.global_agent_env import (
     GlobalAgentHRLProduceCombatUnitPerfectEnv,
     GlobalAgentMultiActionsCombinedRewardEnv)
 from gym_microrts.envs.local_agent_env import LocalAgentEnv
-from gym_microrts.envs.global_agent_mining_hilbert_env import GlobalAgentMiningHilbertEnv
 from gym_microrts.envs.param_op_env import ParamOpEnv
