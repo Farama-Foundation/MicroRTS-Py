daee5628bdb4a273a0aaa1eccdd338df744764ae
Costa Huang <costa.huang@outlook.com>
2020-10-06 21:04:12 -0400

    include microrts directly in the package

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from setuptools import setup
+import setuptools
 
-setup(name='gym_microrts',
-      version='0.1.0',
-      install_requires=['gym', 'dacite', 'jPype1']
+setup(
+    name='gym_microrts',
+    include_package_data=True,
+    version='0.1.0',
+    install_requires=['gym', 'dacite', 'jPype1'],
+    packages=setuptools.find_packages(),
 )
