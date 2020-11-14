7efe20cc3796488193ff65e5615ef99f0150dc6b
Costa Huang <costa.huang@outlook.com>
2020-11-14 18:39:14 -0500

    update example scripts

--- hello_world.py
+++ hello_world.py
@@ -7,20 +7,20 @@ from gym_microrts import microrts_ai
 
 env = gym.make(
     "MicrortsWorkerRush-v1",
-    render_theme=2,
-    frame_skip=9,
-    ai2=microrts_ai.passiveAI,
-    map_path="maps/10x10/basesTwoWorkers10x10.xml",
-    reward_weight=np.array([0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+    render_theme=2, # optional customization
+    frame_skip=2, # optional customization
+    ai2=microrts_ai.workerRushAI, # optional customization
+    map_path="maps/10x10/basesTwoWorkers10x10.xml", # optional customization
+    reward_weight=np.array([0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0]) # optional customization
 )
 
 # env = Monitor(env, f'videos', force=True)
 env.action_space.seed(0)
 env.reset()
-for i in range(10000):
+for i in range(1000):
     env.render()
+    time.sleep(0.03)
     next_obs, reward, done, info = env.step(env.action_space.sample())
     if done:
-        print("done")
-        break
+        env.reset()
 env.close()
--- hello_world_v3.py
+++ hello_world_v3.py
@@ -1,18 +1,31 @@
 import gym
 import gym_microrts
 import time
+import numpy as np
 from gym.wrappers import Monitor
+from gym_microrts import microrts_ai
 
-env = gym.make("MicrortsDefeatCoacAIShaped-v3")
+env = gym.make(
+    "MicrortsDefeatCoacAIShaped-v3",
+    render_theme=2, # optional customization
+    frame_skip=0, # optional customization
+    ai2=microrts_ai.coacAI, # optional customization
+    map_path="maps/16x16/basesWorkers16x16.xml", # optional customization
+    reward_weight=np.array([10.0, 1.0, 1.0, 0.2, 1.0, 4.0, 0.0]) # optional customization
+)
 # env = Monitor(env, f'videos', force=True)
 env.action_space.seed(0)
 env.reset()
 for i in range(10000):
+    env.render()
+    time.sleep(0.001)
     action = env.action_space.sample()
+
+    # optional: selecting only valid units.
     if len((env.unit_location_mask==1).nonzero()[0]) != 0:
         action[0] = (env.unit_location_mask==1).nonzero()[0][0]
+
     next_obs, reward, done, info = env.step(action)
     if done:
-        print("done")
-        break
+        env.reset()
 env.close()
