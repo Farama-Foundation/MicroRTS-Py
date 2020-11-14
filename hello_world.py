62538b6002618884ecc893e59e28dac90e715a8b
Costa Huang <costa.huang@outlook.com>
2020-11-14 00:23:43 -0500

    update hello world files

--- hello_world.py
+++ hello_world.py
@@ -1,21 +1,15 @@
 import gym
 import gym_microrts
 import time
-from gym.wrappers import TimeLimit, Monitor
+from gym.wrappers import Monitor
 
-try:
-    env = gym.make("MicrortsWorkerRush-v1")
-    env = Monitor(env, f'videos', force=True)
-    env.action_space.seed(0)
-    env.reset()
-    for i in range(10000):
-        # time.sleep(0.2)
-        # obs = env.step(env.action_space.sample())
-        next_obs, reward, done, info = env.step(env.action_space.sample())
-        if done:
-            print("done")
-            break
-    #env.close()
-except Exception as e:
-    print(e)
-    print(e.stacktrace())
+env = gym.make("MicrortsWorkerRush-v1")
+# env = Monitor(env, f'videos', force=True)
+env.action_space.seed(0)
+env.reset()
+for i in range(10000):
+    next_obs, reward, done, info = env.step(env.action_space.sample())
+    if done:
+        print("done")
+        break
+env.close()
--- hello_world_v3.py
+++ hello_world_v3.py
@@ -1,24 +1,18 @@
 import gym
 import gym_microrts
 import time
-from gym.wrappers import TimeLimit, Monitor
+from gym.wrappers import Monitor
 
-try:
-    env = gym.make("MicrortsDefeatCoacAIShaped-v3")
-    env = Monitor(env, f'videos', force=True)
-    env.action_space.seed(0)
-    env.reset()
-    for i in range(10000):
-        # time.sleep(0.2)
-        # obs = env.step(env.action_space.sample())
-        action = env.action_space.sample()
-        if len((env.unit_location_mask==1).nonzero()[0]) != 0:
-            action[0] = (env.unit_location_mask==1).nonzero()[0][0]
-        next_obs, reward, done, info = env.step(action)
-        if done:
-            print("done")
-            break
-    env.close()
-except Exception as e:
-    print(e)
-    print(e.stacktrace())
+env = gym.make("MicrortsDefeatCoacAIShaped-v3")
+# env = Monitor(env, f'videos', force=True)
+env.action_space.seed(0)
+env.reset()
+for i in range(10000):
+    action = env.action_space.sample()
+    if len((env.unit_location_mask==1).nonzero()[0]) != 0:
+        action[0] = (env.unit_location_mask==1).nonzero()[0][0]
+    next_obs, reward, done, info = env.step(action)
+    if done:
+        print("done")
+        break
+env.close()
