09b5d7061c74196d3a01e3fcef4bac453fa48137
Costa Huang <costa.huang@outlook.com>
2020-11-14 18:16:38 -0500

    update hello_world

--- hello_world.py
+++ hello_world.py
@@ -2,8 +2,17 @@ import gym
 import gym_microrts
 import time
 from gym.wrappers import Monitor
+from gym_microrts import microrts_ai
 
-env = gym.make("MicrortsWorkerRush-v1")
+env = gym.make(
+    "MicrortsWorkerRush-v1",
+    render_theme=2,
+    frame_skip=9,
+    ai2=microrts_ai.passiveAI,
+    map_path="maps/10x10/basesTwoWorkers10x10.xml",
+    reward_weight=np.array([0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+)
+
 # env = Monitor(env, f'videos', force=True)
 env.action_space.seed(0)
 env.reset()
