19b767af04eca00472174ee100e8d435a19d8616
Costa Huang <costa.huang@outlook.com>
2021-01-16 23:48:26 -0500

    docker update

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,5 +3,8 @@ Xvfb :1 -screen 0 1024x768x24 -ac +extension GLX +render -noreset &> xvfb.log &
 export DISPLAY=:1
 set -e
 wandb login $WANDB
-git pull
+git pull --recurse-submodules
+export JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF8 && \
+cd gym_microrts/microrts && bash build.sh && cd ..&& cd ..
+cd experiments
 exec "$@"
