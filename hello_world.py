d488c59caaf4b9c797fab94864e8b312faa2dae0
Costa Huang <costa.huang@outlook.com>
2021-02-20 13:09:30 -0500

    update

--- hello_world.py
+++ hello_world.py
@@ -1,26 +1,40 @@
+import numpy as np
 import gym
 import gym_microrts
 import time
-import numpy as np
-from gym.wrappers import Monitor
+from gym_microrts.envs.vec_env import MicroRTSVecEnv
 from gym_microrts import microrts_ai
+from gym.envs.registration import register
+from gym_microrts import Config
 
-env = gym.make(
-    "MicrortsWorkerRush-v1",
-    render_theme=2, # optional customization
-    frame_skip=2, # optional customization
-    ai2=microrts_ai.workerRushAI, # optional customization
-    map_path="maps/10x10/basesTwoWorkers10x10.xml", # optional customization
-    reward_weight=np.array([0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0]) # optional customization
-)
-
-# env = Monitor(env, f'videos', force=True)
+try:
+    env = MicroRTSVecEnv(
+        num_envs=1,
+        render_theme=2,
+        ai2s=[microrts_ai.coacAI],
+        map_path="maps/16x16/basesWorkers16x16.xml",
+        reward_weight=np.array([10.0, 1.0, 1.0, 0.2, 1.0, 4.0])
+    )
+    # env = gym.make('MicrortsDefeatCoacAIShaped-v3').env
+    # env = gym.wrappers.RecordEpisodeStatistics(env)
+    # env.action_space.seed(0)
+    obs = env.reset()
+    env.render()
+except Exception as e:
+    e.printStackTrace()
 env.action_space.seed(0)
 env.reset()
-for i in range(1000):
+for i in range(10000):
     env.render()
-    time.sleep(0.03)
-    next_obs, reward, done, info = env.step(env.action_space.sample())
+    action_mask = np.array(env.vec_client.getUnitLocationMasks()).flatten()
+    time.sleep(0.001)
+    action = env.action_space.sample()
+
+    # optional: selecting only valid units.
+    if len(action_mask.nonzero()[0]) != 0:
+        action[0] = action_mask.nonzero()[0][0]
+
+    next_obs, reward, done, info = env.step([action])
     if done:
         env.reset()
 env.close()
