6f41795c14c4dd1a372cbd5191e1ce3ce573ee42
Costa Huang <costa.huang@outlook.com>
2021-03-18 17:46:27 -0400

    quick fix

--- experiments/paper/ppo_gridnet_diverse_encode_decode.py
+++ experiments/paper/ppo_gridnet_diverse_encode_decode.py
@@ -177,9 +177,9 @@ envs = MicroRTSGridModeVecEnv(
     max_steps=2000,
     render_theme=2,
     ai2s=[microrts_ai.coacAI for _ in range(args.num_bot_envs - 6)] + \
-         [microrts_ai.randomBiasedAI for _ in range(2)] + \
-         [microrts_ai.lightRushAI for _ in range(2)] + \
-         [microrts_ai.workerRushAI for _ in range(2)],
+         [microrts_ai.randomBiasedAI for _ in range(min(args.num_bot_envs, 2))] + \
+         [microrts_ai.lightRushAI for _ in range(min(args.num_bot_envs, 2))] + \
+         [microrts_ai.workerRushAI for _ in range(min(args.num_bot_envs, 2))],
     map_path="maps/16x16/basesWorkers16x16.xml",
     reward_weight=np.array([10.0, 1.0, 1.0, 0.2, 1.0, 4.0])
 )
