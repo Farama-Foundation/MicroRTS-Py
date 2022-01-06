fe05e8fa9fc9dd93fbfc05eeea1a53215fecb84f
Costa Huang <costa.huang@outlook.com>
2022-01-06 10:18:46 -0500

    add most important test cases

--- old_tests/test.sh
+++
@@ -1,8 +0,0 @@
-# set -e
-for f in *.py; do
-    echo -e "running $f \n" 
-    python "$f"; 
-    echo -e "\n\n"
-    echo -e "$f finished running"
-    echo "========================================"
-done
