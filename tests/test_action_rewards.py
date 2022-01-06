9c62e40cd2d70b9feca213b5d18c7db9094eccc5
Costa Huang <costa.huang@outlook.com>
2022-01-06 10:13:39 -0500

    refactor

--- tests/test_action_rewards.py
+++ tests/test_action_rewards.py
@@ -45,3 +45,31 @@ def test_resource_gather_reward():
     action[1*num_planes:(1+1)*num_planes] = [3, 0, 0, 2, 0, 0, 0]
     assert envs.step(action)[1].flatten() > 0
     if render: envs.render()
+
+
+
+def test_produce_worker_reward():
+    envs = MicroRTSGridModeVecEnv(
+        num_selfplay_envs=0,
+        num_bot_envs=1,
+        max_steps=2000,
+        render_theme=2,
+        ai2s=[microrts_ai.passiveAI for _ in range(1)],
+        map_paths=["maps/4x4/baseTwoWorkers4x4.xml"],
+        reward_weight=np.array([10.0, 1.0, 1.0, 0.2, 1.0, 4.0])
+    )
+    envs.action_space.seed(0)
+    try:
+        obs = envs.reset()
+        if render: envs.render()
+    except Exception as e:
+        e.printStackTrace()
+    num_planes = len(envs.action_plane_space.nvec)
+
+
+    # produce a worker
+    np.array(envs.get_action_mask())
+    action = np.zeros(len(envs.action_space.nvec), np.int32)
+    action[5*num_planes:(5+1)*num_planes] = [4, 0, 0, 0, 1, 3, 0]
+    assert envs.step(action)[1].flatten() > 0
+    if render: envs.render()
