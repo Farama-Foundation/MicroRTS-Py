418bfd5f0307d1e966b3301cbe52bd400a19da8e
Costa Huang <costa.huang@outlook.com>
2022-01-06 09:51:53 -0500

    refactor test-cases

--- tests/test_resource_gather_reward.py
+++ tests/test_resource_gather_reward.py
@@ -1,38 +1,47 @@
 import numpy as np
-import gym
-import gym_microrts
-from gym.envs.registration import register
-from gym_microrts import Config
 
-gym_id = "GlobalAgentMiningEnv"
-if gym_id not in gym.envs.registry.env_specs:
-    register(
-        gym_id+'-v0',
-        entry_point=f'gym_microrts.envs:{gym_id}',
-        kwargs={'config': Config(
-            frame_skip=9,
-            ai1_type="no-penalty",
-            ai2_type="passive",
-            map_path="maps/4x4/baseTwoWorkers4x4.xml",
-            # below are dev properties
-            microrts_path="~/Documents/work/go/src/github.com/vwxyzjn/microrts",
-        )}
+
+from gym_microrts import microrts_ai
+from gym_microrts.envs.vec_env import MicroRTSGridModeVecEnv
+
+
+def test_resource_gather_reward():
+    envs = MicroRTSGridModeVecEnv(
+        num_selfplay_envs=0,
+        num_bot_envs=1,
+        max_steps=2000,
+        render_theme=2,
+        ai2s=[microrts_ai.passiveAI for _ in range(1)],
+        map_paths=["maps/4x4/baseTwoWorkers4x4.xml"],
+        reward_weight=np.array([10.0, 1.0, 1.0, 0.2, 1.0, 4.0])
     )
-env = gym.make(gym_id+'-v0')
-print(gym_id)
-env.action_space.seed(0)
-try:
-    obs = env.reset(True)
-    env.render()
-except Exception as e:
-    e.printStackTrace()
+    envs.action_space.seed(0)
+    try:
+        obs = envs.reset()
+        envs.render()
+    except Exception as e:
+        e.printStackTrace()
+    num_planes = len(envs.action_plane_space.nvec)
 
-# mine
-assert env.step([1, 2, 0, 3, 0, 0, 0, 0, 0], True)[1] > 0
-env.render()
 
-# return
-assert env.step([1, 3, 0, 0, 2, 0, 0, 0, 0], True)[1] > 0
-env.render()
+    # mine
+    np.array(envs.get_action_mask())
+    action = np.zeros(len(envs.action_space.nvec), np.int32)
+    action[1*num_planes:(1+1)*num_planes] = [2, 0, 3, 0, 0, 0, 0]
+    assert envs.step(action)[1].flatten() > 0
+    envs.render()
 
-env.close()
+    # wait for action to finish
+    for _ in range(20):
+        np.array(envs.get_action_mask())
+        action = np.zeros(len(envs.action_space.nvec), np.int32)
+        envs.step(action)
+        envs.render()
+
+    # return
+    np.array(envs.get_action_mask())
+    action = np.zeros(len(envs.action_space.nvec), np.int32)
+    action[1*num_planes:(1+1)*num_planes] = [3, 0, 0, 2, 0, 0, 0]
+    assert envs.step(action)[1].flatten() > 0
+    envs.render()
+    envs.close()
