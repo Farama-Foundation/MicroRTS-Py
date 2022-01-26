e794ac91c785cbe1828ff82b8b3c7d6bb0a30545
Costa Huang <costa.huang@outlook.com>
2022-01-26 13:48:34 -0500

    isort autoflake

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,12 @@
 import numpy as np
 from numpy.random import choice
-# if you want to record videos, install stable-baselines3 and use its `VecVideoRecorder`
-# from stable_baselines3.common.vec_env import VecVideoRecorder
 
 from gym_microrts import microrts_ai
 from gym_microrts.envs.vec_env import MicroRTSGridModeVecEnv
+
+# if you want to record videos, install stable-baselines3 and use its `VecVideoRecorder`
+# from stable_baselines3.common.vec_env import VecVideoRecorder
+
 
 envs = MicroRTSGridModeVecEnv(
     num_selfplay_envs=0,
@@ -13,29 +15,33 @@ envs = MicroRTSGridModeVecEnv(
     render_theme=2,
     ai2s=[microrts_ai.coacAI for _ in range(1)],
     map_paths=["maps/16x16/basesWorkers16x16.xml"],
-    reward_weight=np.array([10.0, 1.0, 1.0, 0.2, 1.0, 4.0])
+    reward_weight=np.array([10.0, 1.0, 1.0, 0.2, 1.0, 4.0]),
 )
 # envs = VecVideoRecorder(envs, 'videos', record_video_trigger=lambda x: x % 4000 == 0, video_length=2000)
+
 
 def softmax(x):
     """Compute softmax values for each sets of scores in x."""
     e_x = np.exp(x - np.max(x))
     return e_x / e_x.sum()
 
+
 def sample(logits):
     # sample 1 or 2 from logits [0, 1 ,1, 0] but not 0 or 3
-    if sum(logits) == 0: return 0
-    return choice(range(len(logits)), p=logits/sum(logits))
+    if sum(logits) == 0:
+        return 0
+    return choice(range(len(logits)), p=logits / sum(logits))
+
 
 envs.action_space.seed(0)
 envs.reset()
 print(envs.action_plane_space.nvec)
 nvec = envs.action_space.nvec
 
+
 def sample(logits):
-    return np.array(
-        [choice(range(len(item)), p=softmax(item)) for item in logits]
-    ).reshape(-1, 1)
+    return np.array([choice(range(len(item)), p=softmax(item)) for item in logits]).reshape(-1, 1)
+
 
 for i in range(10000):
     envs.render()
@@ -45,16 +51,19 @@ for i in range(10000):
     # but we want to remove PyTorch as a core dependency...
     action_mask = envs.get_action_mask()
     action_mask = action_mask.reshape(-1, action_mask.shape[-1])
-    action_type_mask = action_mask[:,0:6]
-    action = np.concatenate((
-        sample(action_mask[:,0:6]), # action type
-        sample(action_mask[:,6:10]), # move parameter
-        sample(action_mask[:,10:14]), # harvest parameter
-        sample(action_mask[:,14:18]), # return parameter
-        sample(action_mask[:,18:22]), # produce_direction parameter
-        sample(action_mask[:,22:29]), # produce_unit_type parameter
-        sample(action_mask[:,29:sum(envs.action_space.nvec[1:])]), # attack_target parameter
-    ), axis=1)
+    action_type_mask = action_mask[:, 0:6]
+    action = np.concatenate(
+        (
+            sample(action_mask[:, 0:6]),  # action type
+            sample(action_mask[:, 6:10]),  # move parameter
+            sample(action_mask[:, 10:14]),  # harvest parameter
+            sample(action_mask[:, 14:18]),  # return parameter
+            sample(action_mask[:, 18:22]),  # produce_direction parameter
+            sample(action_mask[:, 22:29]),  # produce_unit_type parameter
+            sample(action_mask[:, 29 : sum(envs.action_space.nvec[1:])]),  # attack_target parameter
+        ),
+        axis=1,
+    )
     action = np.array([envs.action_space.sample()])
     next_obs, reward, done, info = envs.step(action)
 envs.close()
--- tests/test_observation.py
+++ tests/test_observation.py
@@ -1,10 +1,9 @@
 import numpy as np
 
-
-from gym_microrts import microrts_ai
 from gym_microrts.envs.vec_env import MicroRTSGridModeVecEnv
 
 render = False
+
 
 def test_observation():
     envs = MicroRTSGridModeVecEnv(
@@ -19,38 +18,146 @@ def test_observation():
     )
 
     next_obs = envs.reset()
-    resource = np.array([
-        0., 1., 0., 0., 0., # 1 hp
-        0., 0., 0., 0., 1., # >= 4 resources
-        1., 0., 0.,         # no owner
-        0., 1., 0., 0., 0., 0., 0., 0.,  # unit type resource
-        1., 0., 0., 0., 0., 0.  # currently not executing actions
-    ]).astype(np.int32)
-    p1_worker = np.array([
-        0., 1., 0., 0., 0., # 1 hp
-        1., 0., 0., 0., 0., # 0 resources
-        0., 1., 0.,         # player 1 owns it 
-        0., 0., 0., 0., 1., 0., 0., 0., # unit type worker
-        1., 0., 0., 0., 0., 0. # currently not executing actions
-    ]).astype(np.int32)
-    p1_base = np.array([
-        0., 0., 0., 0., 1.,  # 1 hp
-        1., 0., 0., 0., 0.,  # 0 resources
-        0., 1., 0.,          # player 1 owns it
-        0., 0., 1., 0., 0., 0., 0., 0., # unit type base
-        1., 0., 0., 0., 0., 0. # currently not executing actions
-    ]).astype(np.int32)
+    resource = np.array(
+        [
+            0.0,
+            1.0,
+            0.0,
+            0.0,
+            0.0,  # 1 hp
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            1.0,  # >= 4 resources
+            1.0,
+            0.0,
+            0.0,  # no owner
+            0.0,
+            1.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,  # unit type resource
+            1.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,  # currently not executing actions
+        ]
+    ).astype(np.int32)
+    p1_worker = np.array(
+        [
+            0.0,
+            1.0,
+            0.0,
+            0.0,
+            0.0,  # 1 hp
+            1.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,  # 0 resources
+            0.0,
+            1.0,
+            0.0,  # player 1 owns it
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            1.0,
+            0.0,
+            0.0,
+            0.0,  # unit type worker
+            1.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,  # currently not executing actions
+        ]
+    ).astype(np.int32)
+    p1_base = np.array(
+        [
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            1.0,  # 1 hp
+            1.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,  # 0 resources
+            0.0,
+            1.0,
+            0.0,  # player 1 owns it
+            0.0,
+            0.0,
+            1.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,  # unit type base
+            1.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,  # currently not executing actions
+        ]
+    ).astype(np.int32)
     p2_worker = p1_worker.copy()
-    p2_worker[10:13] = np.array([0., 0., 1.,]) # player 2 owns it
+    p2_worker[10:13] = np.array(
+        [
+            0.0,
+            0.0,
+            1.0,
+        ]
+    )  # player 2 owns it
     p2_base = p1_base.copy()
-    p2_base[10:13] = np.array([0., 0., 1.,]) # player 2 owns it
-    empty_cell = np.array([
-        1., 0., 0., 0., 0.,  # 0 hp
-        1., 0., 0., 0., 0.,  # 0 resources
-        1., 0., 0.,          # no owner
-        1., 0., 0., 0., 0., 0., 0., 0., # unit type empty cell
-        1., 0., 0., 0., 0., 0. # currently not executing actions
-    ]).astype(np.int32)
+    p2_base[10:13] = np.array(
+        [
+            0.0,
+            0.0,
+            1.0,
+        ]
+    )  # player 2 owns it
+    empty_cell = np.array(
+        [
+            1.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,  # 0 hp
+            1.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,  # 0 resources
+            1.0,
+            0.0,
+            0.0,  # no owner
+            1.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,  # unit type empty cell
+            1.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,  # currently not executing actions
+        ]
+    ).astype(np.int32)
 
     # player 1's perspective
     np.testing.assert_array_equal(next_obs[0][0][0], resource)
@@ -72,11 +179,9 @@ def test_observation():
     # np.testing.assert_array_equal(next_obs[1][14][15], resource) # BUG: in `MicroRTSGridModeVecEnv` the onwer is correctly set to [0, 1, 0]
     np.testing.assert_array_equal(next_obs[1][14][14], p1_worker)
     np.testing.assert_array_equal(next_obs[1][13][13], p1_base)
-    
 
     feature_sum = 0
     for item in [resource, resource, p1_worker, p1_base, resource, resource, p2_worker, p2_base]:
         feature_sum += item.sum()
     feature_sum += empty_cell.sum() * (256 - 8)
     assert next_obs.sum() == feature_sum * 2 == 2560.0
-
