2be3d9bf7019eb61833e6c0f3354bd4b20d39ed7
Costa Huang <costa.huang@outlook.com>
2022-01-28 16:39:55 -0500

    Reduce NN size

--- experiments/league.py
+++ experiments/league.py
@@ -23,12 +23,15 @@ from peewee import (
     SqliteDatabase,
     fn,
 )
-from ppo_gridnet import Agent, MicroRTSStatsRecorder
+from ppo_gridnet_small import Agent, MicroRTSStatsRecorder
 from stable_baselines3.common.vec_env import VecMonitor
 from trueskill import Rating, quality_1vs1, rate_1vs1
 
 from gym_microrts import microrts_ai  # fmt: off
-from gym_microrts.envs.vec_env import MicroRTSBotVecEnv, MicroRTSGridModeVecEnv
+from gym_microrts.envs.vec_env import MicroRTSBotVecEnv
+from gym_microrts.envs.vec_env import (
+    MicroRTSGridModeSharedMemVecEnv as MicroRTSGridModeVecEnv,
+)
 
 
 def parse_args():
