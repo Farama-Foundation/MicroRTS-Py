432e2a161dbbd17c6b887c5d75b7e80eb5747ef2
xluox <wdljwan@gmail.com>
2022-02-23 22:32:28 -0500

    Tied workers to be in the same section as the base.
    Fix the formatting

--- PCG/pcg.py
+++ PCG/pcg.py
@@ -1,17 +1,19 @@
 import random
 import xml.etree.cElementTree as ET
 
-key = 15
-sections = []
-unit_location_records = []
+KEY = 15
+SECTIONS = []
+UNIT_LOCATION_RECORDS = []
+SECTIONS_CHOICES = [0, 1, 2, 3]
+BASE_LOCATION_RECORDS = []
 
 
-def initiateTerrain(root, tag, wallRings):
+def initiate_terrain(root, tag, wallRings):
     terrain = ET.SubElement(root, tag)
     eText = ""
-    global unit_location_records
+    global UNIT_LOCATION_RECORDS
 
-    def getObstacle():
+    def get_obstacle():
         chance = 0.2 * random.random()
         if random.random() < chance:
             return "1"
@@ -22,35 +24,35 @@ def initiateTerrain(root, tag, wallRings):
         for x in range(width):
             if y in range(0, wallRings) or y in range(height - wallRings, height):
                 eText += "1"
-                unit_location_records.append((x, y))
+                UNIT_LOCATION_RECORDS.append((x, y))
             elif x in range(0, wallRings) or x in range(height - wallRings, height):
                 eText += "1"
-                unit_location_records.append((x, y))
+                UNIT_LOCATION_RECORDS.append((x, y))
             else:
-                obstacle = getObstacle()
+                obstacle = get_obstacle()
                 eText += obstacle
                 if obstacle == "1":
-                    unit_location_records.append((x, y))
+                    UNIT_LOCATION_RECORDS.append((x, y))
 
     terrain.text = eText
 
 
-def initiatePlayers(root, tag):
+def initiate_players(root, tag):
     players = ET.SubElement(root, tag)
     ET.SubElement(players, "rts.Player", ID="0", resources="5")
     ET.SubElement(players, "rts.Player", ID="1", resources="5")
 
 
-def initiateUnits(root, tag, wallRings):
+def initiate_units(root, tag, wallRings):
     units = ET.SubElement(root, tag)
-    height = int(root.attrib.get("height"))
-    width = int(root.attrib.get("width"))
-    initiateResources(units, "rts.units.Unit", wallRings, height, width)
-    initiateBases(units, "rts.units.Unit", wallRings, height, width)
-    initiateWorkers(units, "rts.units.Unit", wallRings, height, width)
+    int(root.attrib.get("height"))
+    int(root.attrib.get("width"))
+    initiate_resources(units, "rts.units.Unit")
+    initiate_bases(units, "rts.units.Unit")
+    initiate_workers(units, "rts.units.Unit")
 
 
-def initiateResources(root, tag, wallRings, height, width):
+def initiate_resources(root, tag):
     num_resource = 4
 
     for i in range(num_resource):
@@ -60,41 +62,47 @@ def initiateResources(root, tag, wallRings, height, width):
         )
 
 
-def initiateBases(root, tag, wallRings, height, width):
+def initiate_bases(root, tag):
     num_bases = 2
+    index_list = SECTIONS_CHOICES.copy()
+    global BASE_LOCATION_RECORDS
+
     for i in range(num_bases):
-        x, y = get_xy(random.randint(0, 3))
+        index = random.choice(index_list)
+        BASE_LOCATION_RECORDS.append(index)
+        index_list.remove(index)
+        x, y = get_xy(index)
         ET.SubElement(
             root, tag, type="Base", ID=get_unique_key(), player=str(i % 2), x=str(x), y=str(y), resources="0", hitpoints="10"
         )
 
 
-def initiateWorkers(root, tag, wallRings, height, width):
+def initiate_workers(root, tag):
     num_worker = 2
     for i in range(num_worker):
-        x, y = get_xy(random.randint(0, 3))
+        x, y = get_xy(BASE_LOCATION_RECORDS[i])
         ET.SubElement(
             root, tag, type="Worker", ID=get_unique_key(), player=str(i % 2), x=str(x), y=str(y), resources="0", hitpoints="1"
         )
 
 
 def get_unique_key():
-    global key
-    key = key + 1
-    return str(key)
+    global KEY
+    KEY = KEY + 1
+    return str(KEY)
 
 
 def get_xy(index):
-    global unit_location_records
+    global UNIT_LOCATION_RECORDS
 
-    x, y = random.randint(sections[index][0][0], sections[index][0][1]), random.randint(
-        sections[index][1][0], sections[index][1][1]
+    x, y = random.randint(SECTIONS[index][0][0], SECTIONS[index][0][1]), random.randint(
+        SECTIONS[index][1][0], SECTIONS[index][1][1]
     )
-    while (x, y) in unit_location_records:
-        x, y = random.randint(sections[index][0][0], sections[index][0][1]), random.randint(
-            sections[index][1][0], sections[index][1][1]
+    while (x, y) in UNIT_LOCATION_RECORDS:
+        x, y = random.randint(SECTIONS[index][0][0], SECTIONS[index][0][1]), random.randint(
+            SECTIONS[index][1][0], SECTIONS[index][1][1]
         )
-    unit_location_records.append((x, y))
+    UNIT_LOCATION_RECORDS.append((x, y))
     return x, y
 
 
@@ -107,19 +115,16 @@ if __name__ == "__main__":
     if wallRingsLimit < 0:
         wallRingsLimit = 0
     wallRings = random.randint(0, wallRingsLimit)
-    sections = [
+    SECTIONS = [
         ((wallRings, (width - 1) // 2), (wallRings, (height - 1) // 2)),
         ((width // 2, (width - 1) - wallRings), (wallRings, (height - 1) // 2)),
         ((wallRings, (width - 1) // 2), (height // 2, (height - 1) - wallRings)),
-        ((width // 2, (width - 1) - wallRings),
-         (height // 2, (height - 1) - wallRings)),
+        ((width // 2, (width - 1) - wallRings), (height // 2, (height - 1) - wallRings)),
     ]
 
-    initiateTerrain(root, "terrain", wallRings)
-    initiatePlayers(root, "players")
-    initiateUnits(root, "units", wallRings)
-
-    print(unit_location_records)
+    initiate_terrain(root, "terrain", wallRings)
+    initiate_players(root, "players")
+    initiate_units(root, "units", wallRings)
 
     tree = ET.ElementTree(root)
     tree.write("PCG/maps/filename.xml")
