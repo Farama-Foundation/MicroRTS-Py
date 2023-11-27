6bfb91c6b7798042f2987dbe81a1b71313707a17
DennisSoemers <d.soemers@gmail.com>
2023-11-27 16:06:02 +0100

    also updated botenv

--- gym_microrts/envs/vec_env.py
+++ gym_microrts/envs/vec_env.py
@@ -384,11 +384,11 @@ class MicroRTSBotVecEnv(MicroRTSGridModeVecEnv):
 
         # computed properties
         # [num_planes_hp(5), num_planes_resources(5), num_planes_player(5),
-        # num_planes_unit_type(z), num_planes_unit_action(6)]
+        # num_planes_unit_type(z), num_planes_unit_action(6), num_planes_terrain(2)]
 
-        self.num_planes = [5, 5, 3, len(self.utt["unitTypes"]) + 1, 6]
+        self.num_planes = [5, 5, 3, len(self.utt["unitTypes"]) + 1, 6, 2]
         if partial_obs:
-            self.num_planes = [5, 5, 3, len(self.utt["unitTypes"]) + 1, 6, 2]
+            self.num_planes = [5, 5, 3, len(self.utt["unitTypes"]) + 1, 6, 2, 2]  # 2 extra for visibility
         self.observation_space = gym.spaces.Discrete(2)
         self.action_space = gym.spaces.Discrete(2)
 
